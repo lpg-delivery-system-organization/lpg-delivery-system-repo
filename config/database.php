11893801665633065caea60dc7ccc27296598f72
WinIT102 <winlovepunzalan03@gmail.com>
2026-09-24 16:09:39 +0800

    test changed var (2)

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@
 return [
     'host' => '127.0.0.1',
     'port' => 3306,
-    'dbname' => 'hahahaha',
+    'dbname' => 'hahahaha_dbname',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8mb4',
