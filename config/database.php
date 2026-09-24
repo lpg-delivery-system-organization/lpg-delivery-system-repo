01370a90a09b91c9b890945a64f98cc073b68159
WinIT102 <winlovepunzalan03@gmail.com>
2026-09-24 16:07:01 +0800

    test changed var

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@
 return [
     'host' => '127.0.0.1',
     'port' => 3306,
-    'dbname' => 'lpg_delivery_v2',
+    'dbname' => 'hahahaha',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8mb4',
