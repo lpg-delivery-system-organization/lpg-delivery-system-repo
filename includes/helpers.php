ace402ad7cbb929ee013e8e8cd5d6fa922e1a5e1
WinIT102 <winlovepunzalan03@gmail.com>
2026-09-24 18:53:22 +0800

    changed app.php (2)

--- includes/helpers.php
+++ includes/helpers.php
@@ -40,16 +40,16 @@ function url(string $path = ''): string {
     $baseUrl = defined('BASE_URL') ? BASE_URL : '/lpg-delivery-system-repo';
 
     // Adaptively detect if running under virtualhost root or subfolder
-    if (isset($_SERVER['SCRIPT_NAME']) && isset($_SERVER['HTTP_HOST'])) {
-        $scriptPath = $_SERVER['SCRIPT_NAME'];
-        if (str_starts_with($scriptPath, '/lpg-delivery-system-repo')) {
-            $baseUrl = '/lpg-delivery-system-repo';
-        } elseif (str_starts_with($scriptPath, '/pages/')) {
-            $baseUrl = '';
-        } elseif ($scriptPath === '/index.php' || $scriptPath === '/register.php' || $scriptPath === '/forgot-password.php' || $scriptPath === '/logout.php') {
-            $baseUrl = '';
-        }
-    }
+    // if (isset($_SERVER['SCRIPT_NAME']) && isset($_SERVER['HTTP_HOST'])) {
+    //     $scriptPath = $_SERVER['SCRIPT_NAME'];
+    //     if (str_starts_with($scriptPath, '/lpg-delivery-system-repo')) {
+    //         $baseUrl = '/lpg-delivery-system-repo';
+    //     } elseif (str_starts_with($scriptPath, '/pages/')) {
+    //         $baseUrl = '';
+    //     } elseif ($scriptPath === '/index.php' || $scriptPath === '/register.php' || $scriptPath === '/forgot-password.php' || $scriptPath === '/logout.php') {
+    //         $baseUrl = '';
+    //     }
+    // }
 
     $trimmedBase = rtrim($baseUrl, '/');
 
